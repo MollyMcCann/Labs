2f08d0c1165118f6ebd4e3be5e3679b279f59cd1
Molly McCann <56143091+MollyMcCann@users.noreply.github.com>
2020-03-04 18:56:26 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleMethodsLab
+{
+    class Program
+    {
+        static void Main()
+        {
+            //TODO: Request that a message is to be entered into the Console window
+            //      and leave the cursor on the same line as the request.
+           
+            Console.WriteLine("please enter a message: ");
+            Console.SetCursorPosition(23, 0);
+            //TODO: Read the message from the Console window.
+            Console.ReadLine();
+
+            //TODO: Display the message on the Console window and leave
+            //          the cursor on a new line.
+            Console.WriteLine("Display message \n");
+
+
+            // Keep Console window open to see the output.
+            Console.Write("PRESS <ENTER> to end.");
+            Console.SetCursorPosition(0,4);
+            Console.ReadLine();
+        }
+        // do a method to set the cursor
+        
+
+        protected static int origRow;
+        protected static int origCol;
+
+        protected static void WriteAt(string s, int x, int y)
+        {
+        try
+            {
+            Console.SetCursorPosition(origCol+x, origRow+y);
+            Console.Write(s);
+            }
+        catch (ArgumentOutOfRangeException e)
+            {
+            Console.Clear();
+            Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
