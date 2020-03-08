6b0374c7ba14149043d4231f05868bcdf13148b7
Molly McCann <56143091+MollyMcCann@users.noreply.github.com>
2020-03-07 17:59:24 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,47 +1,26 @@
 ﻿using System;
 
-namespace ConsoleMethodsLab
+namespace DebuggerLab
 {
     class Program
     {
+
         static void Main()
         {
-            //TODO: Request that a message is to be entered into the Console window
-            //      and leave the cursor on the same line as the request.
-           
-            Console.WriteLine("please enter a message: ");
-            Console.SetCursorPosition(23, 0);
-            //TODO: Read the message from the Console window.
-            Console.ReadLine();
+            int valueInt = 23;
+            string valueString = "Hello";
+            bool valueBool = true;
 
-            //TODO: Display the message on the Console window and leave
-            //          the cursor on a new line.
-            Console.WriteLine("Display message \n");
+            // Display value in int variable.
+            Console.WriteLine("Value in int variable is: " + valueInt);
+            
+            Console.WriteLine("Value in string variable is: " + valueString);
 
+            Console.WriteLine("Value in bool variable is: " + valueBool);
 
             // Keep Console window open to see the output.
             Console.Write("PRESS <ENTER> to end.");
-            Console.SetCursorPosition(0,4);
             Console.ReadLine();
-        }
-        // do a method to set the cursor
-        
-
-        protected static int origRow;
-        protected static int origCol;
-
-        protected static void WriteAt(string s, int x, int y)
-        {
-        try
-            {
-            Console.SetCursorPosition(origCol+x, origRow+y);
-            Console.Write(s);
-            }
-        catch (ArgumentOutOfRangeException e)
-            {
-            Console.Clear();
-            Console.WriteLine(e.Message);
-            }
         }
     }
 }
